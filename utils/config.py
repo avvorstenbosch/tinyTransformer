d753c7805db2023d90399478457f6df57a6539e2
avvorstenbosch <alex_van_vorstenbosch@hotmail.com>
2023-03-26 09:45:50 +0200

    moved settings to a config object

---
+++ utils/config.py
@@ -0,0 +1,49 @@
+import torch
+from contextlib import nullcontext
+
+
+class Config:
+    def __init__(
+        self,
+        batch_size=128,  # How many independent sequences will we process in parallel?
+        block_size=256,  # What is the maximum context length for predictions?
+        n_head=6,  # How many self-attention head does a multiheaded self attention block get?
+        n_embed=None,  # How many dimensions do our embeddings have?
+        n_blocks=4,  # How many sequential self-attention blocks does our model get?
+        n_layers=2,  # How many FF-compute layers should the end of a block have?
+        epochs=30,  # For how many epochs will we train the model?
+        steps_per_epoch=10000,  # How many training steps will we take per Epoch?
+        eval_interval=1000,  # How often will we print results for training?
+        learning_rate=1e-4,  # What is our learning rate?
+        eval_iters=200,  # How many samples to use to estimate loss?
+        model_precision=torch.bfloat16,  # Do you want to set the model_precision to float16 to be faster and reduce the memory?
+        compile=True,  # Do you want to compile the model in Pytorch 2.0 to be faster?
+        testrun=False,  # Do you want to test the code on a small dataset?
+        out_dir="./models/",  # Where do you want the output to go?
+        device="cuda" if torch.cuda.is_available() else "cpu",  # Where will we train?
+        precision=None,  # How do you want to set the precision of the model: Either float32 or bfloat16
+        dropout_percentage=0.2,  # How do you want to set the dropout rate?
+    ):
+        self.batch_size = batch_size
+        self.block_size = block_size
+        self.n_head = n_head
+        self.n_embed = n_embed or 32 * n_head
+        self.head_size = self.n_embed // n_head
+        self.n_blocks = n_blocks
+        self.n_layers = n_layers
+        self.epochs = epochs
+        self.steps_per_epoch = steps_per_epoch
+        self.eval_interval = eval_interval
+        self.learning_rate = learning_rate
+        self.eval_iters = eval_iters
+        self.model_precision = model_precision
+        self.compile = compile
+        self.testrun = testrun
+        self.out_dir = out_dir
+        self.device = device
+        self.precision = (
+            nullcontext()
+            if device == "cpu"
+            else torch.amp.autocast(device_type=device, dtype=model_precision)
+        )
+        self.dropout_percentage = dropout_percentage
