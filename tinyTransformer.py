81f567988fa7ae772f32dca7095f7677b148cce6
avvorstenbosch <alex_van_vorstenbosch@hotmail.com>
2023-03-26 09:50:36 +0200

    Updated code to use config, removed training from tinyTransformer.py

--- tinyTransformer.py
+++ tinyTransformer.py
@@ -1,87 +1,6 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-from utils.BPE import BPETokenizer
-import os
-import pickle
-
-# Hyperparameters
-batch_size = 512  # How many independent sequences will we process in parallel?
-block_size = 256  # What is the maximum context length for predictions?
-max_iters = 100000  # How many training steps will we take?
-eval_interval = 250  # How often will we print results for training?
-learning_rate = 3e-4  # What is our learning rate?
-device = "cuda" if torch.cuda.is_available() else "cpu"  # Where will we train?
-eval_iters = 200  # How many samples to use to estimate loss?
-n_head = 4  # How many self-attention head does a multiheaded self attention block get?
-n_embed = 64 * n_head  # How many dimensions do our embeddings have?
-head_size = n_embed  # How many dimensions do Key, Query and Value get?
-n_blocks = 6  # How many sequential self-attention blocks does our model get?
-dropout_percentage = 0.2  # What is de P of dropping out a node?
-# ------------
-
-torch.manual_seed(1337)
-
-print("Fetching data...")
-# get dataset
-paths = os.listdir("./data/maarten/")
-files = []
-for path in paths:
-    with open("./data/maarten/" + path, "r") as file:
-        files.append(file.read().lower())
-
-dataset = ""
-for file in files:
-    dataset += file
-
-
-# here are all the unique characters that occur in this text
-chars = sorted(list(set(dataset)))
-
-# create a mapping from characters to integers
-stoi = {ch: i for i, ch in enumerate(chars)}
-itos = {i: ch for i, ch in enumerate(chars)}
-
-# Load tokenizer
-print("Loading BPETokenizer...")
-with open("./utils/BPE.pickle", "rb") as f:
-    vocab = pickle.load(f)
-    rules = pickle.load(f)
-
-tokenizer = BPETokenizer(vocab, rules)
-vocab_size = len(tokenizer.vocab)
-
-print("Encode Dataset...")
-# Train and test splits
-data = torch.tensor(tokenizer.encode(dataset), dtype=torch.long)
-n = int(0.9 * len(data))  # first 90% will be train, rest val
-train_data = data[:n]
-val_data = data[n:]
-
-# data loading
-def get_batch(split):
-    # generate a small batch of data of inputs x and targets y
-    data = train_data if split == "train" else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i : i + block_size] for i in ix])
-    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
-    x, y = x.to(device), y.to(device)
-    return x, y
-
-
-@torch.no_grad()  # Estimating loss is not a procedure we will backprop over
-def estimate_loss():
-    out = {}
-    model.eval()
-    for split in ["train", "val"]:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            X, Y = get_batch(split)
-            logits, loss = model(X, Y)
-            losses[k] = loss.item()
-        out[split] = losses.mean()
-    model.train()
-    return out
 
 
 class Head(nn.Module):
@@ -89,16 +8,18 @@ class Head(nn.Module):
     A single head of self.attention.
     """
 
-    def __init__(self, head_size):
+    def __init__(self, config):
         super().__init__()
-        self.Key = nn.Linear(n_embed, head_size, bias=False)
-        self.Query = nn.Linear(n_embed, head_size, bias=False)
+        self.Key = nn.Linear(config.n_embed, config.head_size, bias=False)
+        self.Query = nn.Linear(config.n_embed, config.head_size, bias=False)
         # Please note: The head size for the value layer is allowed to be different in size
-        self.Value = nn.Linear(n_embed, head_size, bias=False)
-        self.dropout = nn.Dropout(dropout_percentage)
+        self.Value = nn.Linear(config.n_embed, config.head_size, bias=False)
+        self.dropout = nn.Dropout(config.dropout_percentage)
 
         # register_buffer signals to Torch that this is an externally fed object, not to be optimised
-        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+        self.register_buffer(
+            "tril", torch.tril(torch.ones(config.block_size, config.block_size))
+        )
 
     def forward(self, x):
         B, T, C = x.shape
@@ -126,11 +47,11 @@ class MultiHead(nn.Module):
     Multiple heads of self.attention.
     """
 
-    def __init__(self, num_heads, head_size):
+    def __init__(self, config):
         super().__init__()
-        self.heads = nn.ModuleList(Head(head_size) for _ in range(num_heads))
-        self.projection = nn.Linear(n_embed, n_embed)
-        self.dropout = nn.Dropout(dropout_percentage)
+        self.heads = nn.ModuleList(Head(config) for _ in range(config.n_head))
+        self.projection = nn.Linear(config.n_embed, config.n_embed)
+        self.dropout = nn.Dropout(config.dropout_percentage)
 
     def forward(self, x):
         x = torch.cat([h(x) for h in self.heads], dim=-1)
@@ -144,21 +65,24 @@ class FeedForward(nn.Module):
     A straightforward fully connected layer
     """
 
-    def __init__(self, n_layers=1):
+    def __init__(self, config):
         super().__init__()
         layers = [
-            nn.Linear(n_embed, 4 * n_embed),
+            nn.Linear(config.n_embed, 4 * config.n_embed),
             nn.ReLU(),
         ]
-        for layer in range(n_layers - 1):
+        for layer in range(config.n_layers - 1):
             layers.extend(
                 [
-                    nn.Linear(4 * n_embed, 4 * n_embed),
+                    nn.Linear(4 * config.n_embed, 4 * config.n_embed),
                     nn.ReLU(),
                 ]
             )
         layers.extend(
-            [nn.Linear(4 * n_embed, n_embed), nn.Dropout(dropout_percentage)]
+            [
+                nn.Linear(4 * config.n_embed, config.n_embed),
+                nn.Dropout(config.dropout_percentage),
+            ]
         )  # Add projection for skip-connection
         self.net = nn.Sequential(*layers)
 
@@ -171,14 +95,13 @@ class MultiHeadAttenionBlock(nn.Module):
     Create bloks attention followed by computation
     """
 
-    def __init__(self):
+    def __init__(self, config):
         super().__init__()
-        head_size = n_embed // n_head
-        self.mhsa = MultiHead(num_heads=n_head, head_size=head_size)
-        self.fforward = FeedForward()
+        self.mhsa = MultiHead(config)
+        self.fforward = FeedForward(config)
         # This layer normalization acts on a per token level (on H)
-        self.layernorm1 = nn.LayerNorm(n_embed)
-        self.layernorm2 = nn.LayerNorm(n_embed)
+        self.layernorm1 = nn.LayerNorm(config.n_embed)
+        self.layernorm2 = nn.LayerNorm(config.n_embed)
 
     def forward(self, x):
         # Our forward pass has 2 skip connections implemented
@@ -189,15 +112,19 @@ class MultiHeadAttenionBlock(nn.Module):
 
 # simple GPT-like model, except it's trainable on a single consumer-grade GPU.
 class tinyTransformer(nn.Module):
-    def __init__(self):
+    def __init__(self, config, tokenizer):
         super().__init__()
+        self.config = config
+        self.tokenizer = tokenizer
+        vocab_size = len(tokenizer.vocab)
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
-        self.position_embedding_table = nn.Embedding(block_size, n_embed)
-        blocks = [MultiHeadAttenionBlock() for _ in range(n_blocks)]
+        self.token_embedding_table = nn.Embedding(vocab_size, config.n_embed)
+        self.position_embedding_table = nn.Embedding(config.block_size, config.n_embed)
+        blocks = [MultiHeadAttenionBlock(config) for _ in range(config.n_blocks)]
         self.mhsab = nn.Sequential(*blocks)
-        self.lm_head = nn.Linear(head_size, vocab_size)
-        self.layernorm = nn.LayerNorm(n_embed)
+        self.lm_head = nn.Linear(config.n_embed, vocab_size)
+        self.layernorm = nn.LayerNorm(config.n_embed)
+        self.device = config.device
 
     def forward(self, idx, targets=None):
         B, T = idx.shape  # (B: Batch_size, T: Block_size)
@@ -206,7 +133,7 @@ class tinyTransformer(nn.Module):
         # C : Embedding_size
         token_embed = self.token_embedding_table(idx)  # (B,T,C)
         position_embed = self.position_embedding_table(
-            torch.arange(T, device=device)
+            torch.arange(T, device=self.device)
         )  # (T, C)
         # For every sample from the batch, we broadcast the position embedding
         x = token_embed + position_embed  # (B, T, C)
@@ -228,7 +155,7 @@ class tinyTransformer(nn.Module):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
             # get the predictions
-            logits, loss = self.forward(idx[:, -block_size:])
+            logits, loss = self.forward(idx[:, -self.config.block_size :])
             # focus only on the last time step
             logits = logits[:, -1, :]  # becomes (B, C)
             # apply softmax to get probabilities
@@ -239,57 +166,19 @@ class tinyTransformer(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
         return idx
 
-
-print("Training Model...")
-model = tinyTransformer()
-m = model.to(device)
-
-# create a PyTorch optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
-scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.6)
-
-for iter in range(max_iters):
-
-    # every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0:
-        losses = estimate_loss()
+    def prompt(self, input):
+        """
+        Given your own prompt, generate a text.
+        """
+        context = torch.tensor(
+            self.tokenizer.encode(input), dtype=torch.long, device=self.device
+        ).reshape(1, -1)
         print(
-            f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}\n\n"
-        )
-        context = torch.zeros((1, 1), dtype=torch.long, device=device)
-        print(f"Generative output at step {iter}:")
-        print(
-            tokenizer.decode(m.generate(context, max_new_tokens=100)[0].tolist())
-            + "\n\n"
+            self.tokenizer.decode(
+                self.generate(context, max_new_tokens=256)[0].tolist()
+            )
         )
 
-    # sample a batch of data
-    xb, yb = get_batch("train")
 
-    # evaluate the loss
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
-    if iter % (12 * eval_interval) == 0:
-        scheduler.step()
-
-# generate from the model
-print("Finished training...")
-print("Generative output after training:")
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(tokenizer.decode(m.generate(context, max_new_tokens=100)[0].tolist()))
-
-
-def prompt(input):
-    """
-    Given your own prompt, generate a text.
-    """
-    context = torch.tensor(
-        tokenizer.encode(input), dtype=torch.long, device=device
-    ).reshape(1, -1)
-    print(tokenizer.decode(m.generate(context, max_new_tokens=256)[0].tolist()))
-
-
-# save model
-torch.save(m.state_dict(), "./models/tinyTransformer1.0.torch")
+if __name__ == "__main__":
+    pass
--- train.py
+++ train.py
@@ -1,67 +1,46 @@
 import os
 import pickle
-from contextlib import nullcontext
 
 import torch
-import torch.nn as nn
-from torch.nn import functional as F
 
 from utils.BPE import BPETokenizer
+from utils.save import get_savefile_name
+from utils.config import Config
 from tinyTransformer import tinyTransformer
 
 # -----------------------------------------------------------------------------------#
 #                                 Hyperparameters                                    #
 # -----------------------------------------------------------------------------------#
-batch_size = 128  # How many independent sequences will we process in parallel?
-block_size = 256  # What is the maximum context length for predictions?
-epochs = 30  # For how many epochs will we train the model?
-steps_per_epoch = 10000  # How many training steps will we take per Epoch?
-eval_interval = 250  # How often will we print results for training?
-learning_rate = 1e-4  # What is our learning rate?
-eval_iters = 200  # How many samples to use to estimate loss?
-n_embed = 64 * 8  # How many dimensions do our embeddings have?
-head_size = n_embed  # How many dimensions do Key, Query and Value get?
-n_head = 8  # How many self-attention head does a multiheaded self attention block get?
-n_blocks = 8  # How many sequential self-attention blocks does our model get?
-model_precision = torch.bfloat16  # Do you want to set the model_precision to float16 to be faster and reduce the memory?
-compile = True  # Do you want to compile the model in Pytorch 2.0 to be faster?
+torch.manual_seed(2112)
+config = Config(
+    batch_size=256,  # How many independent sequences will we process in parallel?
+    block_size=256,  # What is the maximum context length for predictions?
+    n_head=6,  # How many self-attention head does a multiheaded self attention block get?
+    n_embed=None,  # How many dimensions do our embeddings have?
+    n_blocks=4,  # How many sequential self-attention blocks does our model get?
+    epochs=30,  # For how many epochs will we train the model?
+    steps_per_epoch=10000,  # How many training steps will we take per Epoch?
+    eval_interval=1000,  # How often will we print results for training?
+    learning_rate=1e-4,  # What is our learning rate?
+    eval_iters=200,  # How many samples to use to estimate loss?
+    model_precision=(
+        torch.bfloat16
+    ),  # Do you want to set the model_precision to float16 to be faster and reduce the memory?
+    compile=True,  # Do you want to compile the model in Pytorch 2.0 to be faster?
+    testrun=False,
+    out_dir="./models/",
+    device="cuda" if torch.cuda.is_available() else "cpu",  # Where will we train?
+    # Set random seed for
+    # Set the processing precision of the model: Either float32 or bfloat16
+    precision=None,  # defaults to bfloat.16
+)
+
 # -----------------------------------------------------------------------------------#
 #                                 Training settings                                  #
 # -----------------------------------------------------------------------------------#
-device = "cuda" if torch.cuda.is_available() else "cpu"  # Where will we train?
-model_precision = (
-    nullcontext()
-    if device == "cpu"
-    else torch.amp.autocast(device_type=device, dtype=model_precision)
-)
-# Set random seed for
-torch.manual_seed(2112)
-# Set the processing precision of the model: Either float32 or bfloat16
-precision = (
-    nullcontext()
-    if device_type == "cpu"
-    else torch.amp.autocast(device_type=device_type, dtype=model_precision)
-)
-# -----------------------------------------------------------------------------------#
-# collect all relevant model creation parameters
-model_args = dict(
-    n_blocks=n_blocks,
-    n_head=n_head,
-    n_embed=n_embed,
-    head_size=head_size,
-    block_size=block_size,
-)  # start with model_args from command line
-# collect all relevant parameters and config settings in this module for saving
-config_keys = [
-    k
-    for k, v in globals().items()
-    if not k.startswith("_") and isinstance(v, (int, float, bool, str))
-]
-config = {k: globals()[k] for k in config_keys}  # will be useful for logging
 # -----------------------------------------------------------------------------------#
 #                                 Load training data                                 #
 # -----------------------------------------------------------------------------------#
-
 print("Fetching data...")
 # get dataset
 paths = os.listdir("./data/maarten/")
@@ -71,14 +50,14 @@ for path in paths:
         files.append(file.read().lower())
 
 dataset = ""
-for file in files:
+length_dataset = len(files) if not config.testrun else 3
+for file in files[:length_dataset]:
     dataset += file
 
 
 # -----------------------------------------------------------------------------------#
 #                           Process training data                                    #
 # -----------------------------------------------------------------------------------#
-
 # here are all the unique characters that occur in this text
 chars = sorted(list(set(dataset)))
 
@@ -106,10 +85,10 @@ val_data = data[n:]
 def get_batch(split):
     # generate a small batch of data of inputs x and targets y
     data = train_data if split == "train" else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i : i + block_size] for i in ix])
-    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
-    x, y = x.to(device), y.to(device)
+    ix = torch.randint(len(data) - config.block_size, (config.batch_size,))
+    x = torch.stack([data[i : i + config.block_size] for i in ix])
+    y = torch.stack([data[i + 1 : i + config.block_size + 1] for i in ix])
+    x, y = x.to(config.device), y.to(config.device)
     return x, y
 
 
@@ -118,31 +97,30 @@ def get_batch(split):
 # -----------------------------------------------------------------------------------#
 
 print("Training Model...")
-model = tinyTransformer()
-m = model.to(device)
+model = tinyTransformer(config, tokenizer)
+m = model.to(config.device)
 
 # compile the model
-if compile:
+if config.compile:
     print("Compiling the model for faster training and inference")
-    unoptimized_model = model
     model = torch.compile(model)  # requires PyTorch 2.0!
 
 # Create an Adam Optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.5)
 
 
 # -----------------------------------------------------------------------------------#
 #                                   Train model                                      #
 # -----------------------------------------------------------------------------------#
-
-
 @torch.no_grad()  # Estimating loss is not a procedure we will backprop over
 def estimate_loss():
+    """Retrieve both a training and a testing batch to estimate the current loss"""
     out = {}
     model.eval()
     for split in ["train", "val"]:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
+        losses = torch.zeros(config.eval_iters)
+        for k in range(config.eval_iters):
             X, Y = get_batch(split)
             logits, loss = model(X, Y)
             losses[k] = loss.item()
@@ -152,16 +130,16 @@ def estimate_loss():
 
 
 # Run our training epochs
-for epoch in epochs:
+for epoch in range(config.epochs):
     # Run each training step per epoch
-    for iter in range(steps_per_epoch):
+    for iter in range(config.steps_per_epoch):
         # Every once in a while evaluate the loss on train and val sets
-        if iter % eval_interval == 0:
+        if iter % config.eval_interval == 0:
             losses = estimate_loss()
             print(
-                f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}\n\n"
+                f"-epoch: {epoch}- step: {iter} | train loss {losses['train']:.4f}, val loss {losses['val']:.4f}\n\n"
             )
-            context = torch.zeros((1, 1), dtype=torch.long, device=device)
+            context = torch.zeros((1, 1), dtype=torch.long, device=config.device)
             print(f"Generative output at step {iter}:")
             print(
                 tokenizer.decode(m.generate(context, max_new_tokens=100)[0].tolist())
@@ -172,38 +150,38 @@ for epoch in epochs:
         xb, yb = get_batch("train")
 
         # evaluate the loss
-        with precision:
+        with config.precision:
             logits, loss = model(xb, yb)
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
 
-        # Save model
-        if epoch > 0:
-            checkpoint = {
-                "model": model.state_dict(),
-                "optimizer": optimizer.state_dict(),
-                "model_args": model_args,
-                "config": config,
-            }
-            print(f"saving checkpoint to {out_dir}")
-            torch.save(checkpoint, os.path.join(out_dir, "ckpt.pt"))
+    # Learning-rate decay
+    scheduler.step()
+
+    # Save model
+    if epoch > 0:
+        checkpoint = {
+            "model": model.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            "config": config,
+        }
+        print(f"saving checkpoint to {config.out_dir}")
+        torch.save(checkpoint, os.path.join(config.out_dir, get_savefile_name(epoch)))
 
 
 # -----------------------------------------------------------------------------------#
 #                                   Save the model                                   #
 # -----------------------------------------------------------------------------------#
-
 torch.save(m.state_dict(), "./models/tinyTransformer1.0.torch")
 
 
 # -----------------------------------------------------------------------------------#
 #                                   Run the model                                    #
 # -----------------------------------------------------------------------------------#
-
 print("Finished training...")
 print("Generative output after training:")
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
+context = torch.zeros((1, 1), dtype=torch.long, device=config.device)
 print(tokenizer.decode(m.generate(context, max_new_tokens=100)[0].tolist()))
 
 
@@ -212,6 +190,6 @@ def prompt(input):
     Given your own prompt, generate a text.
     """
     context = torch.tensor(
-        tokenizer.encode(input), dtype=torch.long, device=device
+        tokenizer.encode(input), dtype=torch.long, device=config.device
     ).reshape(1, -1)
     print(tokenizer.decode(m.generate(context, max_new_tokens=256)[0].tolist()))
